60e34f0952dfb72ed9f3fa06ca5b68e6feac981b
by m.b.y <meyliboy1718@mail.ru>
2023-10-07 09:34:09 +0500

    complete sign up page ui

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,12 +1,12 @@
 import AppRoutes from "../../routes/routes";
-import { Footer, Header } from "../index";
+import { Header } from "../index";
 
 function App() {
   return (
     <>
       <Header />
       <AppRoutes />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
