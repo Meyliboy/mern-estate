396399d38957ee34e9889fa25f4efc9a96b2d759
by m.b.y <meyliboy1718@mail.ru>
2023-11-10 09:03:59 +0500

    bug fix

--- client/src/components/app/App.jsx
+++ client/src/components/app/App.jsx
@@ -1,12 +1,11 @@
-import AppRoutes from "../../routes/routes";
 import { Header } from "../index";
+import AppRoutes from "../../routes/Routes";
 
 function App() {
   return (
     <>
       <Header />
       <AppRoutes />
-      {/* <Footer /> */}
     </>
   );
 }
